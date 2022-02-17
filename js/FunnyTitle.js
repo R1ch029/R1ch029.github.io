621c35f46452b5aada734cc23faf0b828347d454
R1ch029 <1915342979@qq.com>
2022-02-18 00:22:29 +0800

    Site updated: 2022-02-18 00:22:26

---
+++ js/FunnyTitle.js
@@ -0,0 +1,16 @@
+ var OriginTitle = document.title;
+ var titleTime;
+ document.addEventListener('visibilitychange', function () {
+     if (document.hidden) {
+         $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/guixinchn/image/blog/favicon.png");
+         document.title = 'R1ch029';
+         clearTimeout(titleTime);
+     }
+     else {
+         $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/guixinchn/image/blog/favicon.png");
+         document.title = 'R1ch029的博客' + OriginTitle;
+         titleTime = setTimeout(function () {
+             document.title = OriginTitle;
+         }, 2000);
+     }
+ });
